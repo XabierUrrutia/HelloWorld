a3d88b0233ab815a4babbf671e1221b2309eda03
XabierUrrutia <xabier.urrutia@opendeusto.es>
2024-02-23 09:37:10 +0100

    Tren

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace EnemyPlayer{
-    public class Character{
-
-        public string name;
-        private int kills;
-        private int healthPoints;
-
-        public character(string name){
-            this.name = name;
-            healthPoints = 10;
-            Debug.Log("Hola, soy" + name);
-        }
-
-
-        public int getKills(){
-            return kills;
-        }
+public class Character
+{
     
-}
+   public string name;
+   private int healthPoints = 10;
+   public Character(string name)
+       {
+           this.name = name;
+
+       }
+
+       ~Character()
+       {
+           Debug.Log("Destroyed");
+       }
+
+   public int getHealthPoints()
+   {
+       return healthPoints;
+   }
 }
--- Assets/helloWorld.cs
+++ Assets/helloWorld.cs
@@ -1,26 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using EnemyPlayer;
 
-public class helloWorld : MonoBehaviour
+public class HelloWorld : MonoBehaviour
 {
     Enemy enemy;
     Player player;
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
-
-        player = new player("MainCharacter");
-        enemy = new enemy("Enemy");
+        enemy = new Enemy("Voldemort");
+        player = new Player("Harry Potter");
+        Debug.Log("Hello World");
+        Debug.Log("Bye World");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Debug.Log("La salud del jugador es de: " + player);
-        Debug.Log("El jugador ha eliminado: " + player.getKills());
-
+        Debug.Log("Nombre: " + enemy.name + ". Vida: " + enemy.getHealthPoints());
+        Debug.Log("Nombre: " + player.name + ". Vida: " + player.getHealthPoints());
     }
 }
